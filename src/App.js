29c14222655b0399567dbd8fd8123b508e28ca65
arielchm <arielchm@users.noreply.github.com>
2019-11-19 07:53:42 -0400

    2 React Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  foo = () => 'Bars';
+  render() {
+    const name = 'Jhonn Doe';
+    const loading = false;
+    const showName = true;
+    /*
+    if (loading) {
+      return <h4>Loading</h4>;
+    }
+    */
+    return (
+      <div className='App'>
+        <h1>My App</h1>
+        {loading ? <h4>Loading</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;

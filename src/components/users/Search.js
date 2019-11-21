7cab914db2a51a40265cd8ed7685655e684113da
arielchm <arielchm@users.noreply.github.com>
2019-11-20 23:34:12 -0400

    20 Alert Component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -13,15 +13,20 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
   onSubmit = e => {
     // to prevent submitting to a file
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
   render() {
     // If arrow functions are not used 'this' must be passed

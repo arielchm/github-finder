05315c9d5a092aabd51ddb0c7e0d9b956bf4d9d5
arielchm <arielchm@users.noreply.github.com>
2019-11-22 07:37:28 -0400

    30 Create Reducer and Actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,7 @@ import {
   GET_REPOS
 } from '../types';
 
+// State
 const GithubState = props => {
   const initialState = {
     users: [],
@@ -17,9 +18,22 @@ const GithubState = props => {
     repos: [],
     loading: false
   };
+  // Afert making an action an API request will be made, a response will be received and then a type will be dispatched to a reducer
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  const [state, dispath] = useReducer(GithubReducer, initialState);
-  // Search users
+  // Search github users
+  const searchUsers = async text => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+    // setUsers(res.data.items);
+  };
 
   // Get user
 
@@ -28,14 +42,17 @@ const GithubState = props => {
   // Clear users
 
   // Set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
+  // The entire application must be wrapped into a provider, that takes a bunch of props to be available to the entire app
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loadin: state.loading
+        loadin: state.loading,
+        searchUsers
       }}
     >
       {props.children}
--- src/context/types.js
+++ src/context/types.js
@@ -1,3 +1,4 @@
+// Types are relevant to all contexts, they are used to change state within the reducer
 export const SEARCH_USERS = 'SEARCH_USERS';
 export const GET_USERS = 'GET_USERS';
 export const CLEAR_USERS = 'CLEAR_USERS';
